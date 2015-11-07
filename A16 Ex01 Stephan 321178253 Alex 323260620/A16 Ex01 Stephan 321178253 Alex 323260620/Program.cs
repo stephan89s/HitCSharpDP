b32affa6743ab78d85b44ab6f2992f1a46338436
Stephan89s <stephan1989s@gmail.com>
2015-11-07 13:38:35 +0200

    Created new solution

---
+++ A16 Ex01 Stephan 321178253 Alex 323260620/A16 Ex01 Stephan 321178253 Alex 323260620/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace A16_Ex01_Stephan_321178253_Alex_323260620
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
